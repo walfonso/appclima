2173e317dbe7304869be21c1f0a383b25f1e499e
walfonso <walfonso@hotmail.com>
2021-07-12 11:58:49 -0300

    Up app.js consulta API clima y usuario

---
+++ js/app.js
@@ -0,0 +1,114 @@
+var apiKey= '4372ee354f2da2278ed2950dc4c3f288';
+var url2= 'http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=4372ee354f2da2278ed2950dc4c3f288';
+var city = document.querySelector('#ciudad').value;
+var today = document.querySelector('#today');
+var user = document.querySelector('#user');
+var url= `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=4372ee354f2da2278ed2950dc4c3f288`;
+
+
+//console.log(url+apiKey);
+
+function consultUser (){
+  fetch('https://randomuser.me/api')
+  .then(res => res.json())
+  .then(users => {
+    console.log(users)
+    user.innerHTML = `
+      <img src="${users.results['0'].picture.large}" width:"50px" border-radius:"50%"></img>   
+    `
+  })
+};
+function consultApi (){
+try {
+  var cityList = document.querySelector('#cityList');
+  var city = document.querySelector('#ciudad').value;
+  cityList.innerHTML = '';
+  var url= `https://api.openweathermap.org/data/2.5/find?q=${city}&units=metric&lang=sp&appid=4372ee354f2da2278ed2950dc4c3f288`;
+  fetch(url)
+  .then(res => res.json())
+  .then(data => {
+      console.log(data)
+      for (var j = 0; j < data.list.length; j++) {
+      var listItem = document.createElement('li');
+      console.log(listItem);
+      listItem.textContent = data.list[j].name +', '+ data.list[j].sys.country;
+      listItem.setAttribute("value", j ); 
+      cityList.append(listItem);    
+    }
+// Selecciona la ciudad y carga datos columna Izquierda
+var item = document.querySelector('#cityList');
+item.addEventListener("click", function (e) {
+  console.log(e.target.value);
+  console.log(e.target.textContent);
+  var indice = e.target.value;
+  var ciudad = document.querySelector('#ciudad');
+  ciudad.value= e.target.textContent;
+  today.innerHTML = `
+      <img src="https://openweathermap.org/img/wn/${data.list[indice].weather['0'].icon}@2x.png"></img> </p> 
+      <p><h2>${data.list[indice].main.temp}</h2></p> 
+      <p>${data.list[indice].name}, ${data.list[indice].sys.country}</p> 
+      
+    `
+});
+  //cityList.removeChild();
+  })
+ 
+} catch (err) {
+    console.error(err);
+}
+
+};
+
+
+// Pruebas
+function queryClima (){
+var city = document.querySelector('#ciudad').value;
+var url= `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=4372ee354f2da2278ed2950dc4c3f288`;
+  fetch(url)
+  .then(res => res.json())
+  .then(data => {
+    console.log(data)
+    today.innerHTML = `
+      <p>Ciudad:${data.list['2'].name}, ${data.list['2'].sys.country} </p> 
+      <p>Tiempo: <img src="https://openweathermap.org/img/wn/${data.list['2'].weather['0'].icon}@2x.png"></img> </p> 
+      <br></br>
+      <br></br>
+      <p>Temperarura:${data.list['2'].main.temp} </p> 
+      <br></br>
+      <p>Temp. Máxima:${data.list['2'].main.temp_max} </p> <br>
+      <p>Temp. Mínima:${data.list['2'].main.temp_min} </p>
+      
+    `
+    return data;
+  })
+};
+
+//debounce
+//let counter = 0;
+//const getSuggestions = () => {
+//    //Calls API to get Data
+//    console.log("Fetching Data...", counter++);
+//}
+
+//const debounce = function (fn, d) {
+
+function debounce(fn, d) {  
+  let timer;
+    return function () {
+    let context = this, args = arguments;
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+            fn.apply(context, args);
+        }, d)
+    }
+}
+
+const debounceForData = debounce(consultApi, 600);
+
+
+
+
+//console.log(city.value);
+consultUser ();
+consultApi();
+//debounce(consultApi, 600);
