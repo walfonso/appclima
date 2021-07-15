973737c527331ba0064770c9451945fa67e793cd
walfonso <walfonso@hotmail.com>
2021-07-15 01:07:24 -0300

    Mejoras en la busqueda ingremental

--- js/app.js
+++ js/app.js
@@ -1,10 +1,7 @@
-var apiKey= '4372ee354f2da2278ed2950dc4c3f288';
-var url2= 'http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=4372ee354f2da2278ed2950dc4c3f288';
-var city = document.querySelector('#ciudad').value="Rosario";
+var city = document.querySelector('#ciudad').value = "Rosario";
 var today = document.querySelector('#today');
 var user = document.querySelector('#user');
-var url= `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=4372ee354f2da2278ed2950dc4c3f288`;
-var citys = document.querySelector('#citys'); 
+var citys = document.querySelector('#citys');
 var text = document.querySelector('#text');
 var imgcity = document.querySelector('#imgcity');
 var weat = document.querySelector('#weat');
@@ -13,62 +10,63 @@ var visibility = document.querySelector('#visibility');
 var sunrs = document.querySelector('#sunrs');
 var windStatus = document.querySelector('#windStatus');
 var uvindex = document.querySelector('#uvindex');
+
 // Carga datos del usuario
-function queryUser(){
+function queryUser() {
   fetch('https://randomuser.me/api')
-  .then(res => res.json())
-  .then(users => {
-    console.log(users)
-    user.innerHTML = `
+    .then(res => res.json())
+    .then(users => {
+      console.log(users)
+      user.innerHTML = `
       <img src="${users.results['0'].picture.large}" width:"50px" border-radius:"50%"></img>   
     `
-  })
+    })
 };
 
 
 
 // Carga Inicial por defecto Rosario, AR
-function queryDefault (){
+function queryDefault() {
   var city = document.querySelector('#ciudad').value;
   //ciudad.value= e.target.textContent;
-  var url= `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=4372ee354f2da2278ed2950dc4c3f288`;
+  var url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=4372ee354f2da2278ed2950dc4c3f288`;
   var urlpro = `https://api.openweathermap.org/data/2.5/onecall?lat=-32.9468&lon=-60.6393&include=daily&units=metric&appid=4372ee354f2da2278ed2950dc4c3f288`;
   fetch(urlpro)
-  .then(res => res.json())
-  .then(data => {
-    var utcDate = data.current.dt;
-    var fecha = convertDate(utcDate);
-    console.log(data);
-    var sunr = convertTime(data.current.sunrise);
-    var suns = convertTime(data.current.sunset);
-    console.log("dias:"+data.daily.length);  
-    for (var i =1; i < data.daily.length; i++) {
-      var fec = convertDate(data.daily[i].dt);
-      var day = "day"+i;
-      day = document.querySelector('#day'+i);
-      console.log(day); 
-      day.innerHTML = `
-        <p class="descClima">${fec.substr(0,3)}</p>
+    .then(res => res.json())
+    .then(data => {
+      var utcDate = data.current.dt;
+      var fecha = convertDate(utcDate);
+      console.log(data);
+      var sunr = convertTime(data.current.sunrise);
+      var suns = convertTime(data.current.sunset);
+      console.log("dias:" + data.daily.length);
+      for (var i = 1; i < data.daily.length; i++) {
+        var fec = convertDate(data.daily[i].dt);
+        var day = "day" + i;
+        day = document.querySelector('#day' + i);
+        console.log(day);
+        day.innerHTML = `
+        <p class="descClima">${fec.substr(0, 3)}</p>
         <p class="descClima"><img src="https://openweathermap.org/img/wn/${data.daily[i].weather[0].icon}.png"></p>
         <p class="descClima">${data.daily[i].temp.max.toFixed()}º ${data.daily[i].temp.min.toFixed()}º</p>
       `
-    }
+      }
 
-      
-    today.innerHTML = `
+
+      today.innerHTML = `
       <img src="https://openweathermap.org/img/wn/${data.current.weather['0'].icon}@4x.png"></img> 
       <p><h2 class="grados">${data.current.temp.toFixed()} ºC</h2></p> 
       <p><h2 class="fecha">${fecha}</h2></p> 
     `
-    weat.innerHTML = `
+      weat.innerHTML = `
       <img src="https://openweathermap.org/img/wn/${data.current.weather['0'].icon}.png"></img> </p>
       <p>${data.current.weather['0'].description}</p>
     `
-    imgcity.innerHTML = `
+      imgcity.innerHTML = `
       <img src="images/rosario.jpg" />
       <div id="text" class="text"><h2>Rosario, AR</h2></div>
     `
-    uvindex.innerHTML = `
+      uvindex.innerHTML = `
       <p class="descClima text-left color-gris ">UV Index</p>
       <p class="descClima text-left font-3em" style="    padding: 20px 0; margin-bottom: 0px;">${data.current.uvi.toFixed()} %</p>
       <div class="">
@@ -76,22 +74,22 @@ function queryDefault (){
           style="width: 20px;vertical-align: bottom;">
       </div>
     `
-    windstatus.innerHTML = `
+      windstatus.innerHTML = `
       <p class="descClima text-left color-gris ">wind Status</p>
       <p class="descClima text-left font-3em" style="    padding: 20px 0; margin-bottom: 0px;">${data.current.wind_speed} Km/h</p>
       <div class="">
         <p style="margin-top: 0px; font-weight: 500; font-size: 1em">Normal <img src="images/normal.png" alt="Normal"
           style="width: 20px;vertical-align: bottom;">
       </div>
-    `  
-    sunrs.innerHTML = `
+    `
+      sunrs.innerHTML = `
       <p class="descClima text-left color-gris ">Sunrise & Sunset</p>
       <img src="images/sunrise.png" alt="Sunset">
       <p class="descClima text-left font-1em" style="    padding: 20px 0; margin-bottom: 0px;">${sunr}</p>
       <img src="images/sunset.png" alt="Sunset">
       <p class="descClima text-left font-1em" style="    padding: 20px 0; margin-bottom: 0px;">${suns}</p>
     `
-    humidity.innerHTML = `
+      humidity.innerHTML = `
       <p class="descClima text-left color-gris ">Humidity</p>
       <p class="descClima text-left font-3em" style="    padding: 20px 0; margin-bottom: 0px;">${data.current.humidity.toFixed()} %</p>
       <div class="">
@@ -99,7 +97,7 @@ function queryDefault (){
           style="width: 20px;vertical-align: bottom;">
       </div>
     `
-     visibility.innerHTML = `
+      visibility.innerHTML = `
       <p class="descClima text-left color-gris ">visibility</p>
       <p class="descClima text-left font-3em" style="    padding: 20px 0; margin-bottom: 0px;">${data.current.visibility.toFixed()} m</p>
       <div class="">
@@ -107,149 +105,184 @@ function queryDefault (){
           style="width: 20px;vertical-align: bottom;">
       </div>
     `
-  })
+    })
 };
 //
 
-function queryApi (){
-try {
-  var cityList = document.querySelector('#cityList');
+// Busca por lat y lon
+function queryClima(lat, lon) {
   var city = document.querySelector('#ciudad').value;
-  cityList.innerHTML = '';
-  if (city==""){
-    console.log('city vacio');
-    return;
-  }
-  var urlpro = `https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&include=daily&appid=4372ee354f2da2278ed2950dc4c3f288`;
-  var urlweather = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=4372ee354f2da2278ed2950dc4c3f288`;
-  var url= `https://api.openweathermap.org/data/2.5/find?q=${city}&units=metric&lang=sp&appid=4372ee354f2da2278ed2950dc4c3f288`;
-  fetch(url)
-  .then(res => res.json())
-  .then(data => {
-    console.log(data)
-   if ( data.cod==400 || data.cod==401 ){
-      console.log("Vacio");
-      alert("Ciudad no encontrada");
+  var urlpro = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&include=daily&units=metric&appid=4372ee354f2da2278ed2950dc4c3f288`;
+  fetch(urlpro)
+    .then(res => res.json())
+    .then(data => {
+      var utcDate = data.current.dt;
+      var fecha = convertDate(utcDate);
+      console.log(data);
+      var sunr = convertTime(data.current.sunrise);
+      var suns = convertTime(data.current.sunset);
+      console.log("dias:" + data.daily.length);
+      for (var i = 1; i < data.daily.length; i++) {
+        var fec = convertDate(data.daily[i].dt);
+        var day = "day" + i;
+        day = document.querySelector('#day' + i);
+        console.log(day);
+        day.innerHTML = `
+        <p class="descClima">${fec.substr(0, 3)}</p>
+        <p class="descClima"><img src="https://openweathermap.org/img/wn/${data.daily[i].weather[0].icon}.png"></p>
+        <p class="descClima">${data.daily[i].temp.max.toFixed()}º ${data.daily[i].temp.min.toFixed()}º</p>
+      `
+      }
+
+
+      today.innerHTML = `
+      <img src="https://openweathermap.org/img/wn/${data.current.weather['0'].icon}@4x.png"></img> 
+      <p><h2 class="grados">${data.current.temp.toFixed()} ºC</h2></p> 
+      <p><h2 class="fecha">${fecha}</h2></p> 
+    `
+      weat.innerHTML = `
+      <img src="https://openweathermap.org/img/wn/${data.current.weather['0'].icon}.png"></img> </p>
+      <p>${data.current.weather['0'].description}</p>
+    `
+      imgcity.innerHTML = `
+      <img src="images/rosario.jpg" />
+      <div id="text" class="text"><h2>Rosario, AR</h2></div>
+    `
+      uvindex.innerHTML = `
+      <p class="descClima text-left color-gris ">UV Index</p>
+      <p class="descClima text-left font-3em" style="    padding: 20px 0; margin-bottom: 0px;">${data.current.uvi.toFixed()} %</p>
+      <div class="">
+        <p style="margin-top: 0px; font-weight: 500; font-size: 1em">Normal <img src="images/normal.png" alt="Normal"
+          style="width: 20px;vertical-align: bottom;">
+      </div>
+    `
+      windstatus.innerHTML = `
+      <p class="descClima text-left color-gris ">wind Status</p>
+      <p class="descClima text-left font-3em" style="    padding: 20px 0; margin-bottom: 0px;">${data.current.wind_speed} Km/h</p>
+      <div class="">
+        <p style="margin-top: 0px; font-weight: 500; font-size: 1em">Normal <img src="images/normal.png" alt="Normal"
+          style="width: 20px;vertical-align: bottom;">
+      </div>
+    `
+      sunrs.innerHTML = `
+      <p class="descClima text-left color-gris ">Sunrise & Sunset</p>
+      <img src="images/sunrise.png" alt="Sunset">
+      <p class="descClima text-left font-1em" style="    padding: 20px 0; margin-bottom: 0px;">${sunr}</p>
+      <img src="images/sunset.png" alt="Sunset">
+      <p class="descClima text-left font-1em" style="    padding: 20px 0; margin-bottom: 0px;">${suns}</p>
+    `
+      humidity.innerHTML = `
+      <p class="descClima text-left color-gris ">Humidity</p>
+      <p class="descClima text-left font-3em" style="    padding: 20px 0; margin-bottom: 0px;">${data.current.humidity.toFixed()} %</p>
+      <div class="">
+        <p style="margin-top: 0px; font-weight: 500; font-size: 1em">Normal <img src="images/normal.png" alt="Normal"
+          style="width: 20px;vertical-align: bottom;">
+      </div>
+    `
+      visibility.innerHTML = `
+      <p class="descClima text-left color-gris ">visibility</p>
+      <p class="descClima text-left font-3em" style="    padding: 20px 0; margin-bottom: 0px;">${data.current.visibility.toFixed()} m</p>
+      <div class="">
+        <p style="margin-top: 0px; font-weight: 500; font-size: 1em">Normal <img src="images/normal.png" alt="Normal"
+          style="width: 20px;vertical-align: bottom;">
+      </div>
+    `
+    })
+};
+//
+
+function queryApi() {
+  try {
+    var cityList = document.querySelector('#cityList');
+    var city = document.querySelector('#ciudad').value;
+    cityList.innerHTML = '';
+    if (city == "") {
+      console.log('city vacio');
       return;
     }
-  for (var j = 0; j < data.list.length; j++) {
-    var listItem = document.createElement('li');
-    console.log(listItem);
-    listItem.textContent = data.list[j].name +', '+ data.list[j].sys.country;
-    listItem.setAttribute("value", j ); 
-    document.getElementById('citys').style.display ='block';
+    var url = `https://api.openweathermap.org/data/2.5/find?q=${city}&units=metric&lang=sp&appid=4372ee354f2da2278ed2950dc4c3f288`;
+    fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        if (data.cod == 400 || data.cod == 401) {
+          console.log("Vacio");
+          alert("Ciudad no encontrada");
+          return;
+        }
+        for (var j = 0; j < data.list.length; j++) {
+          var listItem = document.createElement('li');
+          console.log(listItem);
+          listItem.textContent = data.list[j].name + ', ' + data.list[j].sys.country;
+          listItem.setAttribute("value", j);
+          document.getElementById('citys').style.display = 'block';
 
-    cityList.append(listItem);    
+          cityList.append(listItem);
+        }
+        console.log("Estoy aca");
+        var item = document.querySelector('#cityList');
+        item.addEventListener("click", function (e) {
+        console.log(e.target.value);
+        console.log(e.target.textContent);
+        var indice = e.target.value;
+        document.getElementById('citys').style.display ='none';
+        var ciudad = document.querySelector('#ciudad');
+        ciudad.value= e.target.textContent;
+        queryClima(data.list[indice].coord.lat, data.list[indice].coord.lon);
+      });
+    })
+  } catch (err) {
+    console.error(err.messaje);
   }
-  // Selecciona la ciudad y carga datos columna Izquierda
-  var item = document.querySelector('#cityList');
-  item.addEventListener("click", function (e) {
-  console.log(e.target.value);
-  console.log(e.target.textContent);
-  var indice = e.target.value;
-  document.getElementById('citys').style.display ='none';
-  //citys.removeChild(cityList);
-
-  //var ciudad = document.querySelector('#ciudad');
-  ciudad.value= e.target.textContent;
-  var utcDate = data.list[indice].dt;
-  var fecha = convertDate(utcDate);
-  console.log("conver:  "+convertDate(utcDate));
-  today.innerHTML = `
-    <img src="https://openweathermap.org/img/wn/${data.list[indice].weather['0'].icon}@4x.png"></img> 
-    <p><h2 class="grados">${data.list[indice].main.temp.toFixed()} ºC</h2></p> 
-    <p><h2 class="fecha">${fecha}</h2></p> 
-     
-  `
-  weat.innerHTML = `
-    <img src="https://openweathermap.org/img/wn/${data.list[indice].weather['0'].icon}.png"></img> </p>
-    <p>${data.list[indice].weather['0'].description}</p>
-  `
-  text.innerHTML = `
-    <h2>${data.list[indice].name}, ${data.list[indice].sys.country} </h2> 
-  `   
-  humidity.innerHTML = `
-    <h2>${data.list[indice].main.humidity} </h2> 
-  `
-
-  visibility.innerHTML = `
-    <h2>${data.list[indice].main.humidity} </h2> 
-  `
-});
-    
-  })
- 
-} catch (err) {
-    console.error(err);
-}
-
 };
 
 
-
-
 //convert date
-function convertDate(UnixTime){
+function convertDate(UnixTime) {
   var unixTimestamp = UnixTime;
-  var date = new Date(unixTimestamp*1000);
+  var date = new Date(unixTimestamp * 1000);
   var weekday = new Array(7);
-  weekday[0] = "Sunday";  
+  weekday[0] = "Sunday";
   weekday[1] = "Monday";
   weekday[2] = "Tuesday";
   weekday[3] = "Wednesday";
   weekday[4] = "Thursday";
   weekday[5] = "Friday";
   weekday[6] = "Saturday";
-//  console.log("Unix Timestamp:",unixTimestamp)
-//  console.log("Date Timestamp:",date.getTime())
   var day = weekday[date.getDay()];
-  var time = date.getHours()+":"+date.getMinutes().toString();
-  var fecha= day+" "+time;
-  return fecha;  
+  var time = date.getHours() + ":" + date.getMinutes().toString();
+  var fecha = day + " " + time;
+  return fecha;
 }
-//
 
 //convert Time
-function convertTime(UnixTime){
+function convertTime(UnixTime) {
   var unixTimestamp = UnixTime;
-  var date = new Date(unixTimestamp*1000);
-  //console.log("Unix Timestamp:",unixTimestamp)
-  //console.log("Date Timestamp:",date.getTime())
-  var time = date.getHours()+":"+date.getMinutes().toString();
-  return time;  
+  var date = new Date(unixTimestamp * 1000);
+  var time = date.getHours() + ":" + date.getMinutes().toString();
+  return time;
 }
-//
-
 
 //debounce
-//let counter = 0;
-//const getSuggestions = () => {
-//    //Calls API to get Data
-//    console.log("Fetching Data...", counter++);
-//}
 
 const debounce = function (fn, d) {
-// 
-  let timer;
-    return function () {
-    let context = this, args = arguments;
+  var timer;
+  return function () {
+    var context = this, args = arguments;
     clearTimeout(timer);
     timer = setTimeout(() => {
-            fn.apply(context, args);
-        }, d)
-    }
+      fn.apply(context, args);
+    }, d)
+  }
 }
-
-
 
 const debounceForData = debounce(queryApi, 600);
 
-
-
-
-//console.log(city.value);
-queryUser ();
-queryDefault ();
-//consultApi();
-//debounce(consultApi, 600);
-//queryClima();
+queryUser();
+queryDefault();
+/*
+setTimeout(() => {
+  var mains = document.getElementById('mains');
+  mains.classList.add('mains');
+}, 3000);
+*/
